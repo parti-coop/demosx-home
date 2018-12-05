829821d064bcd02c157ecde40830ee3759217c72
dalikim <dalikim@parti.xyz>
2018-12-05 16:52:34 +0900

    홈페이지 레이블이 다듬어집니다

--- public/wp-content/themes/flatbase-child/footer.php
+++ public/wp-content/themes/flatbase-child/footer.php
@@ -25,9 +25,16 @@ if ( ! defined( 'WPINC' ) ) {
 	<?php endif; ?>
 
 	<section id="call-to-action" class="home-cta-block clearfix light has-cta-button">
-		<div class="col-full text-align-center">
-			<span class="text-nowrap">시민참여 플랫폼 데모스X는</span> <span class="text-nowrap"><strong><a href="https://democracy.seoul.go.kr/">민주주의서울</a></strong>의 기술 경험과 운영 노하우로</span>
-			<span class="text-nowrap"><strong><a href="http://www.seoul.go.kr/">서울특별시</a></strong>와 <strong><a href="https://partiunion.org">빠띠쿱</a></strong>이</span> <span class="text-nowrap">함께 만들었습니다.</span>
+		<div class="col-full">
+			<div class="cta-wrapper">
+				<div class="cta-text">
+					<span class="text-nowrap">시민참여 플랫폼 데모스X는</span> <span class="text-nowrap"><strong><a href="https://democracy.seoul.go.kr/">민주주의서울</a></strong>의 기술 경험과 운영 노하우로</span>
+					<br>
+					<span class="text-nowrap"><strong><a href="http://www.seoul.go.kr/">서울특별시</a></strong>와 <strong><a href="https://partiunion.org">빠띠쿱</a></strong>이</span> <span class="text-nowrap">함께 만들었습니다.</span>
+				</div>
+				<span class="cta-button-wrapper">
+					<a class="cta-button" href="https://democracy.seoul.go.kr/" title="민주주의서울 바로가기" target="_blank">민주주의서울 바로가기</a>
+				</span>
 		</div>
 	</section>
 
@@ -41,6 +48,7 @@ if ( ! defined( 'WPINC' ) ) {
 						</a>
 					</h4>
 					<div class="textwidget">
+						<p>데모스X는 2018년 행정안전부 주민 체감형 디지털 사회혁신 활성화 사업의 지원으로 제작되었습니다.</p>
 						<p><a href="mailto:contact@demosx.org" style="color: #8b989e">문의 contact@demosx.org</a></p>
 					</div>
 				</div>
